953ac7967ef791d738949a8ffff08a21c41b895e
raulvilasboas97 <raulvilasboas97@gmail.com>
2019-02-19 15:41:07 +0000

    [SPLN] trabalho prático (good state)

--- SPLN/tp1/tp1.py
+++ SPLN/tp1/tp1.py
@@ -55,26 +55,31 @@ def convert_triple(inteiro):
     cent = math.floor(inteiro / 100)
     # para os casos com 100,200,300 ...
     if cent > 0 and resto == 0:
-        string.append(dictc.get(cent))
+        if cent == 1:
+            string.append('cem')
+        else:
+            string.append(dictc.get(cent))
     # para os casos com 111,153,251 ...
     elif cent > 0:
         string.append(str(dictc.get(cent)) + " e")
-    # elif resto != 0 and cent != 0:
-    # para os casos com 11,12,13 ...
-    if resto < 20 and resto >= 10:
-        string.append(dicte.get(resto))
+    elif resto == 0 and cent == 0:
+        pass
     else:
-        dezena = math.floor(resto/10)
-        unidade = resto % 10
-        # para o caso com 09
-        if dezena == 0 and unidade > 0:
-            string.append(dictu.get(unidade))
-        # para o caso com 20
-        elif dezena > 0 and unidade == 0:
-            string.append(dictd.get(dezena))
-        # para o caso com 29
+        # para os casos com 11,12,13 ...
+        if resto < 20 and resto >= 10:
+            string.append(dicte.get(resto))
         else:
-            string.append( str(dictd.get(dezena)) + " e " + str(dictu.get(unidade)) )
+            dezena = math.floor(resto/10)
+            unidade = resto % 10
+            # para o caso com 09
+            if dezena == 0 and unidade > 0:
+                string.append(dictu.get(unidade))
+            # para o caso com 20
+            elif dezena > 0 and unidade == 0:
+                string.append(dictd.get(dezena))
+            # para o caso com 29
+            else:
+                string.append( str(dictd.get(dezena)) + " e " + str(dictu.get(unidade)) )
     return ((' ').join(string))
 
 def converter(numero):
@@ -84,14 +89,26 @@ def converter(numero):
     for i in reversed(range(0,len(lst))):
         inteiro = int(lst[i])
         if i==0:
-            numero_str.append(convert_triple(inteiro))
+            # para o caso de ser apenas um '0'
+            if len(lst) == 1 and inteiro == 0:
+                numero_str.append('zero')
+            # para o restos dos numeros na 1ª casa
+            else:
+                numero_str.append(convert_triple(inteiro))
         elif i==1:
-            if inteiro != 0:
+            # caso especial de '1 000' em que é apenas mil
+            if inteiro == 1:
+                numero_str.append("mil ")
+            # para os outros casos
+            elif inteiro > 1:
                 numero_str.append(convert_triple(inteiro) + " mil e")
+            # caso seja tenha '000' não faz nada
         elif i==2:
+            # casos normais
             if inteiro > 1:
                 numero_str.append(convert_triple(inteiro) + " milhões")
-            else:
+            # caso especial de '1 000 000' em que é apenas 'um milhão'
+            elif inteiro==1:
                 numero_str.append(convert_triple(inteiro) + " milhão")
 
     numero_str = ' '.join(numero_str)
