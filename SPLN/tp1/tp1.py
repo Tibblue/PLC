92d38c219fe22e94296ab8d4022b4f1353619f1a
raulvilasboas97 <raulvilasboas97@gmail.com>
2019-02-26 18:34:52 +0000

    [SPLN] take it all

--- SPLN/tp1/tp1.py
+++ SPLN/tp1/tp1.py
@@ -1,53 +1,10 @@
 import math
+import numeros
 
-dictu =  {
-    1: 'um',
-    2: 'dois',
-    3: 'três',
-    4: 'quatro',
-    5: 'cinco',
-    6: 'seis',
-    7: 'sete',
-    8: 'oito',
-    9: 'nove',
-}
-
-dictd = {
-    1: 'dez',
-    2: 'vinte',
-    3: 'trinta',
-    4: 'quarenta',
-    5: 'cinquenta',
-    6: 'sessenta',
-    7: 'setenta',
-    8: 'oitenta',
-    9: 'noventa',
-}
-
-dicte = {
-    10: 'dez',
-    11: 'onze',
-    12: 'doze',
-    13: 'treze',
-    14: 'quatorze',
-    15: 'quinze',
-    16: 'dezasseis',
-    17: 'dezassete',
-    18: 'dezoito',
-    19: 'dezenove',
-}
-
-dictc =  {
-    1: 'cento',
-    2: 'duzentos',
-    3: 'trezentos',
-    4: 'quatrocentos',
-    5: 'quinhentos',
-    6: 'seiscentos',
-    7: 'setecentos',
-    8: 'oitocentos',
-    9: 'novecentos',
-}
+dictu = numeros.dictu
+dictd = numeros.dictd
+dicte = numeros.dicte
+dictc = numeros.dictc
 
 # cent -> representa a casa das centenas do triplo
 # resto -> resto da divisão do triplo
@@ -127,8 +84,8 @@ def converter(numero):
             # caso especial de '1 000 000' em que é apenas 'um milhão'
             elif inteiro==1:
                 numero_str.append(convert_triple(inteiro) + " milhão")
-        elif i==3:
-            numero_str.append(convert_triple(inteiro) + " mil milhões")
+        # elif i==3:
+        #     numero_str.append(convert_triple(inteiro) + " mil milhões")
 
     numero_str = ' '.join(numero_str).capitalize()
 
@@ -139,7 +96,7 @@ def converter(numero):
 # converter('000')
 
 converter('3 244')
-converter('123 001 123')
+# converter('123 001 123')
 
 def teste():
     for i in range(1,100):
