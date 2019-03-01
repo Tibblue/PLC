b8a1136bec446536b81f8ce6a7b1b4356e77521e
KIKO <fjmo_1997@hotmail.com>
2019-03-01 15:25:04 +0000

    [SPLN] tp1 faz coisas, melhor ou pior

--- SPLN/tp1/tp1aLaEnunciado.py
+++ SPLN/tp1/tp1aLaEnunciado.py
@@ -52,7 +52,7 @@ def convert_triple(inteiro):
     return (' ').join(string)
 
 
-def converter(numero):
+def converterNum2Text(numero):
     # print(numero) # debug do input
     numero_str = []
     lst = numero.split(',')
@@ -70,22 +70,16 @@ def converter(numero):
                 numero_str.append('zero' + " vírgula")
             else:
                 numero_str.append(convert_triple(inteiro) + " vírgula")
-
     numero_str = ' '.join(numero_str).capitalize()
     return numero_str
 
-# print(converter('3,333,244'))
-# print(converter('0,000'))
-# print(converter('000'))
-# print(converter('100,000,010'))
-
-
-def converterInput(numero):
+def converterNum2TextREGEX(numero):
     # print(numero) # debug do input
-    # print(converter(str(numero.group(0))))
-    return " >>"+converter(numero.group(0))+"<< "
+    result = converterNum2Text(numero.group(0))
+    # return result # sem cor
+    return colored(result,'white','on_blue') # com cor
 
-def converterAnos(ano):
+def converterNum2TextAnos(ano):
     ano = int(ano.group())
     milhares = math.floor(ano / 1000)
     triplo = ano % 1000
@@ -93,20 +87,38 @@ def converterAnos(ano):
         milhares="Mil"
     else:
         milhares="Dois mil"
-    return "__"+milhares+" e "+converter(str(triplo))+"__"
+    result = milhares+" e "+converterNum2Text(str(triplo))
+    # return result # sem cor
+    return colored(result, 'white','on_green') # com cor
+
+def converterNum2Text_file(filename):
+    print(colored(" -> NUM 2 TEXT -> "+filename,"red"))
+    inputFile = open(filename, "r").read()
+    # print(inputFile)
+    input = inputFile
+    input = re.sub(r'\d{4}',converterNum2TextAnos,input)
+    input = re.sub(r'(\d{1,3},)*\d{1,3}(?=[ %])',converterNum2TextREGEX,input)
+    print(input)
+
+# traduzir o ficheiro de teste
+converterNum2Text_file("A/example_input.txt")
 
 
 
-print(" NUM 2 TEXT")
-inputFile = open("A/example_input.txt", "r").read()
-# print(inputFile)
-input = inputFile
-input = re.sub(r'\d{4}',converterAnos,input)
-input = re.sub(r'(\d{1,3},)*\d{1,3}(?=[ %])',converterInput,input)
-print(input)
 
 
 def text2num(texto):
+    texto = texto.group(1)
+    if texto in dictInv:
+        # return '>>'+str(dictInv[texto])+'<<'
+        return colored(str(dictInv[texto]), 'white','on_blue')
+        # return "YEY » "+texto+" -> "+str(dictInv[texto])
+    else:
+        return texto
+        # return colored(texto, 'green')
+        # return ' '.join(["SAD",texto])
+
+def text2numAno(texto):
     texto = texto.group(1)
     if texto in dictInv:
         # return '>>'+str(dictInv[texto])+'<<'
@@ -115,19 +127,39 @@ def text2num(texto):
         return texto
         # return colored(texto, 'green')
 
-print(" TEXT 2 NUM")
+
+# print(colored(" TEXT 2 NUM","red"))
+# outputFile = open("A/example_output.txt", "r").read()
+# output = outputFile
+# print(output)
+# output = re.sub(r'(\w+) mil e',text2num,output)
+# output = re.sub(r'(\w+)(?: e )?',text2num,output)
+# output = re.sub(r' vírgula ',colored(',','white','on_blue'),output)
+# print(output)
+
+
+
+
+
+
+
+dicKeys = '|'.join(dictInv)
+# print(dicKeys)
+# regEx = "("+dicKeys+"+"+" )"+"(?:e[ ,])?" # works... kinda
+regEx = "(?<=\\b)"+"(?:("+dicKeys+")\\b(?: e | )?)"+"(?= |\.)" # better version ? or not
+print(regEx)
+
+def colorirN(text):
+    return colored(text.group(),'white','on_blue')
+def colorirA(text):
+    return colored(text.group(),'white','on_green')
+
+print(colored(" TEXT 2 NUM colorir","yellow"))
 outputFile = open("A/example_output.txt", "r").read()
 output = outputFile
-print(output)
-output = re.sub(r'(\w+) mil e',text2num,output)
-output = re.sub(r'(\w+)(?: e )?',text2num,output)
-output = re.sub(r' vírgula ',colored(',','white','on_blue'),output)
+# print(output)
+# output = re.sub(r''+dicKeys+'+'+' mil e '+regEx,colorirA,output)
+output = re.sub(r''+regEx,colorirN,output)
+output = re.sub(r' vírgula ',colored(',','white','on_magenta'),output)
 print(output)
 
-def teste():
-    for i in range(1,100):
-        s = ' '.join([str(i),'000'])
-        # print(s)
-        converter(s)
-
-# teste()
