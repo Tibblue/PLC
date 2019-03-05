207daf657ec62097c8ae78812bb2772d8abb11d4
raulvilasboas97 <raulvilasboas97@gmail.com>
2019-03-05 13:51:49 +0000

    [LEI] bot adivinhas começado

--- LEI/codigo/bot_adivinhas/bot_adivinhas.py
+++ LEI/codigo/bot_adivinhas/bot_adivinhas.py
@@ -1,24 +1,58 @@
 import random
 from listaAdivinhas import adivinhas
+import re
 
-def getAdivinha():
+
+
+def getAdivinhaResposta():
     listaAdvinhas = adivinhas
-    print(listaAdvinhas)
+    size = len(listaAdvinhas)-1
+    n = random.randint(0,size)
+    adivinha = listaAdvinhas[n].capitalize()
+    adivinha = re.search(r'(.*)(?:::)(.*)', adivinha)
+    adiv = adivinha.group(1)
+    resposta = adivinha.group(2)
+    return (adiv,resposta)
 
-getAdivinha()
 
-
-
+def verificaResposta(adivinha,resposta):
+    mensagem = input("Eu:")
+    if(resposta in mensagem):
+        print('Está certooooo')
+    else:
+        print('Falhaste. Queres tentar outra vez ou queres a resposta?')
+        mensagem = input('Eu: ')
+        mensagem = re.search(r'.*(resposta|sim|não).*', mensagem,re.IGNORECASE)
+        captured = mensagem.group(1).capitalize()
+        if not mensagem is None and (captured == 'Resposta' or captured == 'Não'):
+            print("A resposta era " + resposta + ".")
+        elif not mensagem is None and captured == 'Sim':
+            print(adivinha)
+            verificaResposta(adivinha,resposta)
 
 def talk():
     # art = text2art("")
     # print(art)
-    mensagem = input("Eu: ")
-    if mensagem == 'Quero um adivinha':
-        while True:
-            mensagem = input("Eu:")
+    while True:
+        mensagem = input("Eu: ")
+        if mensagem == 'Quero uma adivinha':
+            (adivinha,resposta) = getAdivinhaResposta()
+            print(adivinha)
+            verificaResposta(adivinha,resposta)
+            # mensagem = input("Eu:")
+            # if(resposta in mensagem):
+            #     print('Está certooooo')
+            # else:
+            #     print('Falhaste. Queres tentar outra vez ou queres a resposta?')
+            #     mensagem = input('Eu: ')
+            #     msg = re.search(r'.*(resposta).*', mensagem)
+            #     if not msg is None and msg.group(1) == 'resposta':
+            #         print("A resposta era " + resposta + ".")
+            #     else:
+            #         msg = re.search(r'.*(sim|Sim).*', mensagem)
+            #         print('Vai força')
 
-
+talk()
 
         # mensagem = "a" # debug
         # result = getProverbios(mensagem)
