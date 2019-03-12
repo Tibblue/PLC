930f2f52913a3b501c59a2117f08dc37704fb6e1
KIKO <fjmo_1997@hotmail.com>
2019-03-12 15:50:56 +0000

    [LEI] fase inicial da cache no bot_tradutor

--- LEI/codigo/diretor/bot_tradutor/bot_tradutor.py
+++ LEI/codigo/diretor/bot_tradutor/bot_tradutor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import re
 import random
+import json
 from art import *
 from py_translator import Translator
 
@@ -14,22 +15,37 @@ matchFailed = ['Essa não sei.','Está fora dos meus conhecimentos.','Não perce
 ##### Funçoes #####
 # traduz uma dada palavra para uma dada linguagem
 def traduz(palavra,linguagem):
-    if linguas.get(linguagem) is not None:
-        abrevLinguagem = linguas.get(linguagem)
-        result = Translator().translate(palavra,abrevLinguagem).text
-        result = "A tradução de " + palavra + " é " + result + "."
-        return result
+    abrevLinguagem = linguas.get(linguagem)
+    if abrevLinguagem is not None:
+        traducao = Translator().translate(palavra,abrevLinguagem).text
+        resposta = "A tradução de " + palavra + " é " + traducao + "."
+        guardar(palavra,abrevLinguagem,traducao)
+        return resposta
     else: # nao encontrou a lingua
         return None
         # return random.choice(linguaNotFound) #resposta de falha
 
+def guardar(palavra,linguagem,traducao):
+    # carrega o json e converte para um dict (nao parece mas é)
+    python_obj = json.loads(open("./diretor/bot_tradutor/cache.json").read())
+        # print(python_obj)
+        # print(python_obj["kiko"]) # fails miserably
+        # print(python_obj.get('kiko')) # retorna None
+        # print(python_obj["carro"]["en"]) # info aninhada
+    python_obj.update({palavra:{linguagem:traducao}}) # atualiza/adiciona novo elemento
+    # abre ficheiro para escrita
+    f = open("./diretor/bot_tradutor/cache.json", "w")
+    # prettyfy do JSON
+    prettyJSON = json.dumps(python_obj,sort_keys=True, indent=2)
+    f.write(prettyJSON)
+
 # gera regras de uso do bot para o diretor
 def geraRegras():
     regras = [
-        # ( r'(.+) em (.+)', lambda x: bot_tradutor.traduz(x.group(1),x.group(2).capitalize())), # regra antiga
         # ( r'(?:.* )?(.+) em (\w+)\b\??', lambda x: bot_tradutor.traduz(x.group(1),x.group(2).capitalize())),
         ( r'como se diz ([\w ]+) em (\w+)\b\??', lambda x: bot_tradutor.traduz(x.group(1),x.group(2).capitalize())),
-        ( r'em (\w+) como se diz (\w+)\b\??', lambda x: bot_tradutor.traduz(x.group(2),x.group(1).capitalize())),
+        ( r'em (\w+) como se diz ([\w ]+)\b\??', lambda x: bot_tradutor.traduz(x.group(2),x.group(1).capitalize())),
+        ( r'([\w ]+) em (\w+) diz-se ([\w ]+)\b\??', lambda x: bot_tradutor.guardar(x.group(1),x.group(2),x.group(3))),
     ]
     return regras
 
