d1913d59257f927c394647c9104a31c10c889032
KIKO <fjmo_1997@hotmail.com>
2019-03-20 13:31:27 +0000

    [LEI] botmix removed (old diretor)

--- LEI/codigo/bot_mix/myDicio.py
+++
@@ -1,6 +0,0 @@
-respostasFeitas = [
-    "Não encontrei nada. :(", "Não tenho mais nada a dizer!" ,
-    "Eu sou apenas um pobre bot.",
-    "Não conheço nenhum provérbio para isso.",
-    "Manda vir outro!",
-]
