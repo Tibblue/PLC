49b2d8a19cf7f42afc69ac17553bffb058b622d8
raulvilasboas97 <raulvilasboas97@gmail.com>
2019-03-24 18:44:06 +0000

    [LEI] bot_csv no diretor, acho que não tem bugs

--- LEI/codigo/diretor/bot_csv/bot_csv.py
+++ LEI/codigo/diretor/bot_csv/bot_csv.py
@@ -1,17 +1,21 @@
 import csv
 import re
 import json
-
+import os
 ########################################################################
 # funções para carregar o ficheiro json e o csv
 
 # faz o open do json e guarda
-def save_json():
-    json_file = json.loads(open("files_csv.json").read())
+def save_json(path_files_csv):
+    json_file = json.loads(open(path_files_csv).read())
     return json_file
 
+# pathX = os.getcwd()
+# print(pathX)
+# x = os.path.dirname(os.path.normpath(pathX))
+# print(x)
 # retorna uma lista com o nome das colunas do csv
-def get_lista_nomeColunass(json_file):
+def get_lista_nomeColunas(json_file):
     dict_keys_nomeColuna = json_file['individuals.csv'].keys()
     lista_nomeColunas = []
     for nome_coluna in dict_keys_nomeColuna:
@@ -41,23 +45,8 @@ lista_questao_tipo = [
     ('Qual','Objeto')
 ]
 
-json_file = save_json()
-lista_nomeColunass = get_lista_nomeColunass(json_file)
-valores_csv = openCSV('individual.csv')
-nome_colunas_exp_reg = '|'.join(lista_nomeColunass)
 
-
-# tuplo com a exp reg e o número de matches que vai dar
-lista_exp_reg = [
-    (r'(Quem).* (.*) anos\b\??'), # ver como resolver esta situação
-    (r'(Quem).* (.*)\b\??'),
-    (r'(Quantos).* (.*)\b\??'),
-    (r'(Onde).* (.*)\b\??'),
-    (r'(Quando).* (.*)\b\??'),
-    (r'(Qual).*('+nome_colunas_exp_reg+r').* (.*)\b\??')
-]
-
-def mensagemSearch(mensagem,lista_nomeColunass):
+def mensagemSearch(mensagem,lista_nomeColunas,lista_exp_reg):
     questao = ""
     nome_coluna_obj = ""
     elemento = ""
@@ -83,39 +72,67 @@ def findRow(elemento,valores_csv):
 
 
 # responde quando tem todos os argumentos precisos
-def respond_full_agr(nome_coluna_obj,elemento,lista_nomeColunass):
+def respond_full_agr(nome_coluna_obj,elemento,lista_nomeColunas,valores_csv):
     row = findRow(elemento,valores_csv)
-    posi = lista_nomeColunass.index(nome_coluna_obj.capitalize())
+    posi = lista_nomeColunas.index(nome_coluna_obj.capitalize())
     resposta = row[posi]
     return resposta
 
 # responde quando não tem todos os argumentos precisos
-def respond_missing_agr(questao,elemento,lista_nomeColunass):
+def respond_missing_agr(questao,elemento,lista_nomeColunas,files_csv,valores_csv):
     for quest,tipo in lista_questao_tipo:
         if quest.lower() == questao:
             tipo_obj = tipo
-    for nomeColuna in lista_nomeColunass:
-        tipo_coluna = json_file['individuals.csv'][nomeColuna]['Tipo']
+    for nomeColuna in lista_nomeColunas:
+        tipo_coluna = files_csv['individuals.csv'][nomeColuna]['Tipo']
         if tipo_obj == tipo_coluna:
             nome_coluna_obj = nomeColuna
 
-    resposta = respond_full_agr(nome_coluna_obj,elemento,lista_nomeColunass)
+    resposta = respond_full_agr(nome_coluna_obj,elemento,lista_nomeColunas,valores_csv)
     return resposta
 
-def talk():
-    while True:
-        mensagem = input('Eu: ')
-        # mensagem = 'Qual a comida preferida do Kiko?'
-        # mensagem = 'Quem nasceu em Portugal?'
-        # mensagem = 'Quando nasceu o Kiko?'
+def responde(mensagem,files_csv):
 
-        (questao,nome_coluna_obj,elemento) = mensagemSearch(mensagem,lista_nomeColunass)
-        print(questao,nome_coluna_obj,elemento)
+    # path para ser possível ir buscar coisas à data
+    path_atual = os.getcwd()
+    path_data = path_atual + '/diretor/data/'
 
-        if nome_coluna_obj is not "":
-            resposta = respond_full_agr(nome_coluna_obj,elemento,lista_nomeColunass)
-        else:
-            resposta = respond_missing_agr(questao,elemento,lista_nomeColunass)
-        print(resposta)
+    # path para o ficheiro json que contem todos os csv
+    path_files_csv = path_data + files_csv
 
-talk()
+    # conteudo do ficheiro json
+    files_csv = save_json(path_files_csv)
+
+    # lista com os nomes das colunas
+    lista_nomeColunas = get_lista_nomeColunas(files_csv)
+
+    '''
+    Waning o files_csv.json vai permitir ter vários csv´s por isso isto depois tem de
+    ser alterado de forma a permitir isso
+    Para já só está feito para dar parau um csv especifico
+    '''
+    path_individual = path_data + '/individual.csv'
+
+    # retorna o contéudo do csv especifico
+    valores_csv = openCSV(path_individual)
+
+    nome_colunas_exp_reg = '|'.join(lista_nomeColunas)
+
+    # tuplo com a exp reg e o número de matches que vai dar
+    lista_exp_reg = [
+        (r'(Quem).* (.*) anos\b\??'), # ver como resolver esta situação
+        (r'(Quem).* (.*)\b\??'),
+        (r'(Quantos).* (.*)\b\??'),
+        (r'(Onde).* (.*)\b\??'),
+        (r'(Quando).* (.*)\b\??'),
+        (r'(Qual).*('+nome_colunas_exp_reg+r').* (.*)\b\??')
+    ]
+
+    (questao,nome_coluna_obj,elemento) = mensagemSearch(mensagem,lista_nomeColunas,lista_exp_reg)
+    # print(questao,nome_coluna_obj,elemento)
+
+    if nome_coluna_obj is not "":
+        resposta = respond_full_agr(nome_coluna_obj,elemento,lista_nomeColunas,valores_csv)
+    else:
+        resposta = respond_missing_agr(questao,elemento,lista_nomeColunas,files_csv,valores_csv)
+    return resposta
