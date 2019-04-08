f3496c962efda10ff859400a8ba93545e0dfdd26
KIKO <fjmo_1997@hotmail.com>
2019-04-08 16:45:16 +0100

    [PRC] aula8 done (controller incompleto)

--- PRC/kiko/aulas/aula8/cinemaREST/app.js
+++ PRC/kiko/aulas/aula8/cinemaREST/app.js
@@ -4,8 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var cinemaRouter = require('./routes/cinema');
 
 var app = express();
 
@@ -19,8 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', cinemaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +33,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
+  console.log('ERROR')
 });
 
 module.exports = app;
