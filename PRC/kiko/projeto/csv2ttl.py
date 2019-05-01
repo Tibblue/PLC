78709a23ab29474d6b3fa0e58f42952319265d6e
KIKO <fjmo_1997@hotmail.com>
2019-05-01 17:06:44 +0100

    [PRC] good advances at the ontology and script
    script
    - classe anime e director
    - relaçao hasDirector
    turtle
    - conversao esta ok
    - muito pode ser adicionado

--- PRC/kiko/projeto/csv2ttl.py
+++ PRC/kiko/projeto/csv2ttl.py
@@ -9,7 +9,7 @@ csv_reader = csv.reader(csv_file, delimiter=',')
 line_count = 0
 
 # printa erros para STDERR
-def printERR(*args, **kwargs):
+def printE(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 # resolve problemas com ids
@@ -19,7 +19,7 @@ def fix_id(id):
     id = re.sub(r"(\W)",r"_",id) # replace nao palavras (aka simbolos mostly)
     id = re.sub(r"^(\d)",r"_\1",id) # coloca _ caso o id comece por um numero
     id = re.sub(r"_+","_",id) # remove excesso de _
-    # printERR(id)
+    # printE(id)
     return id
 
 ontologia = """
@@ -32,6 +32,16 @@ ontologia = """
     @base <http://www.semanticweb.org/kiko/ontologies/2019/projeto> .
 
     <http://www.semanticweb.org/kiko/ontologies/2019/projeto> rdf:type owl:Ontology .
+
+    #################################################################
+    #    Object Properties
+    #################################################################
+
+    ###  http://www.semanticweb.org/kiko/ontologies/2019/projeto#hasDirector
+    :hasDirector rdf:type owl:ObjectProperty ;
+                rdfs:domain :Anime ;
+                rdfs:range :Director .
+
 
     #################################################################
     #    Data properties
@@ -58,12 +68,18 @@ ontologia = """
     :Anime rdf:type owl:Class .
 
 
+    ###  http://www.semanticweb.org/kiko/ontologies/2019/projeto#Director
+    :Director rdf:type owl:Class .
+
+
     #################################################################
     #    Individuals
     #################################################################
 """
 print(ontologia)
 
+
+directors = []
 for row in csv_reader:
     line_count += 1
     if line_count == 1:
@@ -72,14 +88,40 @@ for row in csv_reader:
         pass
     else:
         id = row[0].split("http://dbpedia.org/resource/")[1]
-        id = fix_id(id)
+        id = "ANIME_"+fix_id(id)
         print("###  http://www.semanticweb.org/kiko/ontologies/2019/projeto#"+id)
         print(f':{id} rdf:type owl:NamedIndividual, :Anime.')
         if row[1]!="NULL":
             print(f':{id} :label "{row[1]}" .')
-        if row[2]!="NULL":
-            print(f':{id} :director_label "{row[2]}" .')
-        if row[3]!="NULL":
-            print(f':{id} :director "{row[3]}" .')
+            # else: colocar o ID qd nao houver label?
+        if row[2]!="NULL" and row[3]!="NULL":
+            r2 = re.search(r"{(.*)}",row[2])
+            r3 = re.search(r"{(.*)}",row[3])
+            if r2 and r3:
+                s2 = r2.group(1).split('|')
+                s3 = r3.group(1).split('|')
+                for i in range(len(s2)):
+                    # print(f':{id} :director_label "{s2[i]}" .')
+                    print(f':{id} :hasDirector :{"DIRECTOR_"+fix_id(s3[i].split("http://dbpedia.org/resource/")[1])} .')
+                    directors.append((s3[i],s2[i]))
+            else:
+                # print(f':{id} :director_label "{row[2]}" .')
+                print(f':{id} :hasDirector :{"DIRECTOR_"+fix_id(row[3].split("http://dbpedia.org/resource/")[1])} .')
+                directors.append((row[3],row[2]))
         print()
-print(f'# Processed {line_count} lines.')
+# printE(f'# Processed {line_count} lines.')
+
+
+directors.sort()
+directors = set(directors)
+print("\n\n#####  DIRECTORS  #####\n")
+for director,label in directors:
+    # print(id,label)
+    id = director.split("http://dbpedia.org/resource/")[1]
+    id = "DIRECTOR_"+fix_id(id)
+    print("###  http://www.semanticweb.org/kiko/ontologies/2019/projeto#"+id)
+    print(f':{id} rdf:type owl:NamedIndividual, :Director.')
+    print(f':{id} :director "{director}".')
+    print(f':{id} :label "{label}".')
+    print()
+    pass
