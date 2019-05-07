5126ed355577b64bac15e715e6017eef8186def4
KIKO <fjmo_1997@hotmail.com>
2019-05-07 11:54:11 +0100

    [PRC] upgrades de node

--- PRC/kiko/projeto/web-sparql/routes/sparql.js
+++ PRC/kiko/projeto/web-sparql/routes/sparql.js
@@ -8,7 +8,7 @@ var router = express.Router();
 var endpoints = [
     'https://dbpedia.org/sparql',
     'https://query.wikidata.org/sparql',
-    'http://localhost:4005/sparql'
+    'http://localhost:7200/repositories/projetoBeta'
 ];
 
 
@@ -40,14 +40,44 @@ router.post('/', function(req, res, next) {
 
     console.log('Dataset: ' + endpoint);
     console.log('Output format: ' + output);
-    console.log('Query: ' + query);
-    console.log('Encoded: ' + encoded);
+    console.log('Query: \n' + query);
+    console.log('Encoded: \n' + encoded);
 
     axios.get(endpoint + '?query=' + encoded, { headers: { Accept: 'application/sparql-results+' + output } })
         .then(response => {
             res.jsonp(response.data);
         })
         .catch(err => console.log('ERRO: ' + err));
+});
+
+/* GET users listing. */
+router.get('/graphDB/sparqlQuery/:output/', function(req, res, next) {
+    var endpoint = endpoints[2];
+    var encoded = req.query.query
+    var query = decodeURIComponent(encoded)
+    var output = req.params.output
+    console.log(output)
+    if (output != 'json' && output !='xml')
+        res.send("Pls use JSON or XML.\nAppreciated")
+
+    console.log('Dataset: ' + endpoint);
+    console.log('Output format: ' + output);
+    console.log('Query: \n' + query);
+    // console.log('Encoded: \n' + encoded);
+
+    axios.get(endpoint + '?query=' + encoded, {
+        headers: {
+            // Accept: 'application/sparql-results+json'
+            // Accept: 'application/sparql-results+xml'
+            // Accept: 'application/x-binary-rdf-results-table'
+            Accept: 'application/sparql-results+' + output
+        }
+    })
+    .then(response => {
+        res.send(response.data);
+        // res.jsonp(response.data);
+    })
+    .catch(err => console.log('ERRO: ' + err));
 });
 
 module.exports = router;
