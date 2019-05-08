989cae7a7b4e7ed2cd04273ad38563b84210e642
KIKO <fjmo_1997@hotmail.com>
2019-05-08 11:59:17 +0100

    [PRC] routes a usar querysSaved no graphDB

--- PRC/kiko/projeto/backend/routes/querys.js
+++ PRC/kiko/projeto/backend/routes/querys.js
@@ -13,10 +13,10 @@ router.get('/', function(req, res, next) {
     res.send('Give a query name');
 });
 
+
 /* GET getClasses. */
 router.get('/getClasses', function(req, res, next) {
-    var query = 'select * where { ?s a owl:Class }'
-    var encoded = encodeURIComponent(query)
+    var encoded = encodeURIComponent('select * where { ?s a owl:Class }')
 
     console.log('PEDIDO: getClasses')
     // console.log('Query: \n' + query);
@@ -29,31 +29,48 @@ router.get('/getClasses', function(req, res, next) {
     .catch(err => console.log('ERRO: ' + err));
 });
 
-/* GET getDirectorsTop10MostAnimes. */
-router.get('/getDirectorsTop10MostAnimes', function (req, res, next) {
-    var query = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX : <http://www.semanticweb.org/kiko/ontologies/2019/projeto#>
-        select distinct ?director (COUNT(?anime) AS ?nAnimes) where {
-            ?director rdf:type :Director .
-            ?anime :hasDirector ?director .
-        }
-        GROUP BY ?director
-        ORDER by desc(?nAnimes)
-        LIMIT 10`
-    var encoded = encodeURIComponent(query)
 
-    console.log(query)
-    console.log(encoded)
+/* GET getDirectorsNumAnimes (Saved Query) */
+router.get('/getDirectorsNumAnimes', function(req, res, next) {
+    var url = 'http://localhost:7200/rest/sparql/saved-queries?name='
+    var encodedName = encodeURIComponent('PRC-Proj_director nAnimes')
+    console.log(encodedName)
+    axios.get(url+encodedName)
+        .then(response => {
+            var query = response.data.body
+            var encoded = encodeURIComponent(query)
+            console.log('PEDIDO: getDirectorsNumAnimes (Saved Query)')
+            // console.log('Query: \n' + response.data.body)
 
-    console.log('PEDIDO: getDirectorsTop10MostAnimes')
-    // console.log('Query: \n' + query);
-    // console.log('Encoded: \n' + encoded);
-
-    axios.get(endpoint + '?query=' + encoded, {
-        headers: { Accept: 'application/sparql-results+json' }
-    })
-    .then(response => res.jsonp(response.data))
-    .catch(err => console.log('ERRO: ' + err));
+            axios.get(endpoint + '?query=' + encoded, {
+                headers: { Accept: 'application/sparql-results+json' }
+            })
+            .then(response => res.jsonp(response.data))
+            .catch(err => console.log('ERRO: ' + err));
+        })
+        .catch(err => console.log('ERRO: ' + err))
 });
+
+/* GET getNumClasses (Saved Query) */
+router.get('/getNumClasses', function(req, res, next) {
+    var url = 'http://localhost:7200/rest/sparql/saved-queries?name='
+    var encodedName = encodeURIComponent('conta classes')
+    axios.get(url+encodedName)
+        .then(response => {
+            var query = response.data.body
+            var encoded = encodeURIComponent(query)
+            console.log('PEDIDO: getNumClasses (Saved Query)')
+            // console.log('Query: \n' + response.data.body)
+
+            axios.get(endpoint + '?query=' + encoded, {
+                headers: { Accept: 'application/sparql-results+json' }
+            })
+            .then(response => res.jsonp(response.data))
+            .catch(err => console.log('ERRO: ' + err));
+        })
+        .catch(err => console.log('ERRO: ' + err))
+});
+
+
 
 module.exports = router;
