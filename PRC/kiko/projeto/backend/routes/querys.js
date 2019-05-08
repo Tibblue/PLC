7b866aade626a85063e622a4b5e5503130fd82f1
KIKO <fjmo_1997@hotmail.com>
2019-05-08 12:39:46 +0100

    [PRC] node - this is good enough for now
    -index de savedQuerys
    -retorna em json pedidos

--- PRC/kiko/projeto/backend/routes/querys.js
+++ PRC/kiko/projeto/backend/routes/querys.js
@@ -5,17 +5,52 @@ var path = require('path');
 var router = express.Router();
 
 /* Lista de endpoints. */
+/* GraphDB endpoint */
 var endpoint = 'http://localhost:7200/repositories/projetoBeta'
 
 
-/* GET input page. */
+/* GET Saved Querys list interface. */
 router.get('/', function(req, res, next) {
-    res.send('Give a query name');
+    // res.send('Give a query name');
+    var url = 'http://localhost:7200/rest/sparql/saved-queries'
+    axios.get(url)
+        .then(response => {
+            console.log('PEDIDO: GraphDB Saved Querys')
+            var html = '<h2>GraphDB Saved Querys</h2>'+'<ol>'
+            for (i in response.data) {
+                var name = response.data[i].name
+                html = html.concat('<li><a href="http://localhost:4005/query/'+name+'">' + name + '</li></a>')
+            }
+            html = html+'</ol>'
+            res.send(html)
+        })
+        .catch(err => console.log('ERROR returning Saved Querys list : ' + err))
+});
+
+/* GET any savedQuery. */
+router.get('/:savedQuery', function(req, res, next) {
+    var url = 'http://localhost:7200/rest/sparql/saved-queries?name='
+    var encodedName = encodeURIComponent(req.params.savedQuery)
+    console.log(encodedName)
+    axios.get(url+encodedName)
+        .then(response => {
+            var query = response.data.body
+            var encoded = encodeURIComponent(query)
+            console.log('PEDIDO: '+encodedName+' (Saved Query auto)')
+            // console.log('Query: \n' + response.data.body)
+
+            axios.get(endpoint + '?query=' + encoded, {
+                headers: { Accept: 'application/sparql-results+json' }
+            })
+            .then(response => res.jsonp(response.data))
+            .catch(err => console.log('ERRO: ' + err));
+        })
+        .catch(err => console.log('ERRO: ' + err))
 });
 
 
 /* GET getClasses. */
-router.get('/getClasses', function(req, res, next) {
+router.get('/others/getClasses', function(req, res, next) {
     var encoded = encodeURIComponent('select * where { ?s a owl:Class }')
 
     console.log('PEDIDO: getClasses')
@@ -29,32 +64,10 @@ router.get('/getClasses', function(req, res, next) {
     .catch(err => console.log('ERRO: ' + err));
 });
 
-
-/* GET getDirectorsNumAnimes (Saved Query) */
-router.get('/getDirectorsNumAnimes', function(req, res, next) {
-    var url = 'http://localhost:7200/rest/sparql/saved-queries?name='
-    var encodedName = encodeURIComponent('PRC-Proj_director nAnimes')
-    console.log(encodedName)
-    axios.get(url+encodedName)
-        .then(response => {
-            var query = response.data.body
-            var encoded = encodeURIComponent(query)
-            console.log('PEDIDO: getDirectorsNumAnimes (Saved Query)')
-            // console.log('Query: \n' + response.data.body)
-
-            axios.get(endpoint + '?query=' + encoded, {
-                headers: { Accept: 'application/sparql-results+json' }
-            })
-            .then(response => res.jsonp(response.data))
-            .catch(err => console.log('ERRO: ' + err));
-        })
-        .catch(err => console.log('ERRO: ' + err))
-});
-
 /* GET getNumClasses (Saved Query) */
-router.get('/getNumClasses', function(req, res, next) {
+router.get('/others/getNumClasses', function (req, res, next) {
     var url = 'http://localhost:7200/rest/sparql/saved-queries?name='
-    var encodedName = encodeURIComponent('conta classes')
+    var encodedName = encodeURIComponent('conta_classes')
     axios.get(url+encodedName)
         .then(response => {
             var query = response.data.body
