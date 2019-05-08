c2aa22487788e5c685d45bc64abe03019aa1d211
KIKO <fjmo_1997@hotmail.com>
2019-05-08 20:39:33 +0100

    [PRC] minor tweak

--- PRC/kiko/projeto/backend/routes/querys.js
+++ PRC/kiko/projeto/backend/routes/querys.js
@@ -10,77 +10,77 @@ var endpoint = 'http://localhost:7200/repositories/projetoBeta'
 
 /* GET Saved Querys list interface. */
 router.get('/', function(req, res, next) {
-    // res.send('Give a query name');
-    var url = 'http://localhost:7200/rest/sparql/saved-queries'
-    axios.get(url)
-        .then(response => {
-            console.log('PEDIDO: GraphDB Saved Querys')
-            var html = '<h2>GraphDB Saved Querys</h2>'+'<ol>'
-            for (i in response.data) {
-                var name = response.data[i].name
-                html = html.concat('<li><a href="http://localhost:4005/query/'+name+'">' + name + '</li></a>')
-            }
-            html = html+'</ol>'
-            res.send(html)
-        })
-        .catch(err => console.log('ERROR returning Saved Querys list : ' + err))
+  // res.send('Give a query name');
+  var url = 'http://localhost:7200/rest/sparql/saved-queries'
+  axios.get(url)
+    .then(response => {
+      console.log('PEDIDO: GraphDB Saved Querys')
+      var html = '<h2>GraphDB Saved Querys</h2>'+'<ol>'
+      for (i in response.data) {
+        var name = response.data[i].name
+        html = html.concat('<li><a href="http://localhost:4005/query/'+name+'">' + name + '</li></a>')
+      }
+      html = html+'</ol>'
+      res.send(html)
+    })
+    .catch(err => console.log('ERROR returning Saved Querys list : ' + err))
 });
 
 /* GET any savedQuery. */
 router.get('/:savedQuery', function(req, res, next) {
-    var url = 'http://localhost:7200/rest/sparql/saved-queries?name='
-    var encodedName = encodeURIComponent(req.params.savedQuery)
-    // console.log(encodedName)
-    axios.get(url+encodedName)
-        .then(response => {
-            var query = response.data.body
-            var encoded = encodeURIComponent(query)
-            console.log('PEDIDO: '+encodedName+' (Saved Query auto)')
-            // console.log('Query: \n' + response.data.body)
+  var url = 'http://localhost:7200/rest/sparql/saved-queries?name='
+  var encodedName = encodeURIComponent(req.params.savedQuery)
+  // console.log(encodedName)
+  axios.get(url+encodedName)
+    .then(response => {
+      var query = response.data.body
+      var encoded = encodeURIComponent(query)
+      console.log('PEDIDO: '+encodedName+' (Saved Query auto)')
+      // console.log('Query: \n' + response.data.body)
 
-            axios.get(endpoint + '?query=' + encoded, {
-                headers: { Accept: 'application/sparql-results+json' }
-            })
-            .then(response => res.jsonp(response.data))
-            .catch(err => console.log('ERRO: ' + err));
-        })
-        .catch(err => console.log('ERRO: ' + err))
+      axios.get(endpoint + '?query=' + encoded, {
+        headers: { Accept: 'application/sparql-results+json' }
+      })
+      .then(response => res.jsonp(response.data))
+      .catch(err => console.log('ERRO: ' + err));
+    })
+    .catch(err => console.log('ERRO: ' + err))
 });
 
 
 /* GET getClasses. */
 router.get('/others/getClasses', function(req, res, next) {
-    var encoded = encodeURIComponent('select * where { ?s a owl:Class }')
+  var encoded = encodeURIComponent('select * where { ?s a owl:Class }')
 
-    console.log('PEDIDO: getClasses')
-    // console.log('Query: \n' + query);
-    // console.log('Encoded: \n' + encoded);
+  console.log('PEDIDO: getClasses')
+  // console.log('Query: \n' + query);
+  // console.log('Encoded: \n' + encoded);
 
-    axios.get(endpoint + '?query=' + encoded, {
-        headers: { Accept: 'application/sparql-results+json' }
-    })
-    .then(response => res.jsonp(response.data))
-    .catch(err => console.log('ERRO: ' + err));
+  axios.get(endpoint + '?query=' + encoded, {
+    headers: { Accept: 'application/sparql-results+json' }
+  })
+  .then(response => res.jsonp(response.data))
+  .catch(err => console.log('ERRO: ' + err));
 });
 
 /* GET getNumClasses (Saved Query) */
 router.get('/others/getNumClasses', function (req, res, next) {
-    var url = 'http://localhost:7200/rest/sparql/saved-queries?name='
-    var encodedName = encodeURIComponent('conta_classes')
-    axios.get(url+encodedName)
-        .then(response => {
-            var query = response.data.body
-            var encoded = encodeURIComponent(query)
-            console.log('PEDIDO: getNumClasses (Saved Query)')
-            // console.log('Query: \n' + response.data.body)
+  var url = 'http://localhost:7200/rest/sparql/saved-queries?name='
+  var encodedName = encodeURIComponent('conta_classes')
+  axios.get(url+encodedName)
+    .then(response => {
+      var query = response.data.body
+      var encoded = encodeURIComponent(query)
+      console.log('PEDIDO: getNumClasses (Saved Query)')
+      // console.log('Query: \n' + response.data.body)
 
-            axios.get(endpoint + '?query=' + encoded, {
-                headers: { Accept: 'application/sparql-results+json' }
-            })
-            .then(response => res.jsonp(response.data))
-            .catch(err => console.log('ERRO: ' + err));
-        })
-        .catch(err => console.log('ERRO: ' + err))
+      axios.get(endpoint + '?query=' + encoded, {
+        headers: { Accept: 'application/sparql-results+json' }
+      })
+      .then(response => res.jsonp(response.data))
+      .catch(err => console.log('ERRO: ' + err));
+    })
+    .catch(err => console.log('ERRO: ' + err))
 });
 
 
--- PRC/kiko/projeto/backend/routes/sparql.js
+++ PRC/kiko/projeto/backend/routes/sparql.js
@@ -6,79 +6,81 @@ var router = express.Router();
 
 /* Lista de endpoints. */
 var endpoints = [
-    'https://dbpedia.org/sparql',
-    'https://query.wikidata.org/sparql',
-    'http://localhost:7200/repositories/projetoBeta'
+  'https://dbpedia.org/sparql',
+  'https://query.wikidata.org/sparql',
+  'http://localhost:7200/repositories/projetoBeta'
 ];
 
 
 /* GET input page. */
 router.get('/', function(req, res, next) {
-    res.render('getInput');
+  res.render('getInput');
 });
 
 /* POST input query. */
 router.post('/', function(req, res, next) {
-    var dataset = req.body.dataset;
-    var output = req.body.output;
-    var query = req.body.intext;
-    var encoded = encodeURIComponent(query);
+  var dataset = req.body.dataset;
+  var output = req.body.output;
+  var query = req.body.intext;
+  var encoded = encodeURIComponent(query);
 
-    var endpoint = '';
-    switch (dataset) {
-        case 'dbpedia':
-            endpoint = endpoints[0];
-            break;
-        case 'wikidata':
-            endpoint = endpoints[1];
-            break;
-        case 'localhost':
-            endpoint = endpoints[2];
-            break;
-        default:
-            endpoint = 'ERROR!';
-    }
+  var endpoint = '';
+  switch (dataset) {
+    case 'dbpedia':
+      endpoint = endpoints[0];
+      break;
+    case 'wikidata':
+      endpoint = endpoints[1];
+      break;
+    case 'localhost':
+      endpoint = endpoints[2];
+      break;
+    default:
+      endpoint = 'ERROR!';
+  }
 
-    console.log('Dataset: ' + endpoint);
-    console.log('Output format: ' + output);
-    console.log('Query: \n' + query);
-    console.log('Encoded: \n' + encoded);
+  console.log('Dataset: ' + endpoint);
+  console.log('Output format: ' + output);
+  console.log('Query: \n' + query);
+  console.log('Encoded: \n' + encoded);
 
-    axios.get(endpoint + '?query=' + encoded, { headers: { Accept: 'application/sparql-results+' + output } })
-        .then(response => {
-            res.jsonp(response.data);
-        })
-        .catch(err => console.log('ERRO: ' + err));
+  axios.get(endpoint + '?query=' + encoded, { headers: { Accept: 'application/sparql-results+' + output } })
+    .then(response => {
+      res.jsonp(response.data);
+    })
+    .catch(err => console.log('ERRO: ' + err));
 });
 
 /* GET query API. */
-router.get('/graphDB/sparqlQuery/:output/', function(req, res, next) {
-    var endpoint = endpoints[2];
-    var encoded = req.query.query
-    var query = decodeURIComponent(encoded)
-    var output = req.params.output
-    console.log(output)
-    if (output != 'json' && output !='xml')
-        res.send("Pls use JSON or XML.\nAppreciated")
+router.get('/sparqlQuery/', function(req, res, next) {
+  var endpoint = endpoints[2];
+  var encoded = req.query.query
+  var query = decodeURIComponent(encoded)
+  var output = req.query.output
 
-    console.log('Dataset: ' + endpoint);
-    console.log('Output format: ' + output);
-    console.log('Query: \n' + query);
-    // console.log('Encoded: \n' + encoded);
+  if(output==undefined)
+    output = 'json'
+  if (output != 'json' && output !='xml')
+    res.send("Pls use JSON or XML.\nAppreciated")
 
-    axios.get(endpoint + '?query=' + encoded, {
-        headers: {
-            // Accept: 'application/sparql-results+json'
-            // Accept: 'application/sparql-results+xml'
-            // Accept: 'application/x-binary-rdf-results-table'
-            Accept: 'application/sparql-results+' + output
-        }
-    })
-    .then(response => {
-        res.send(response.data);
-        // res.jsonp(response.data);
-    })
-    .catch(err => console.log('ERRO: ' + err));
+  console.log('Dataset: ' + endpoint);
+  console.log('Output format: ' + output);
+  console.log('Query: \n' + query);
+  // console.log('Encoded: \n' + encoded);
+
+  axios.get(endpoint + '?query=' + encoded, {
+    headers: {
+      Accept: 'application/sparql-results+' + output
+      // Accept: 'application/sparql-results+json'
+      // Accept: 'application/sparql-results+xml'
+      // Accept: 'application/x-binary-rdf-results-table'
+    }
+  })
+  .then(response => {
+    res.send(response.data);
+    // res.jsonp(response.data);
+  })
+  .catch(err => console.log('ERRO: ' + err));
 });
 
 module.exports = router;
