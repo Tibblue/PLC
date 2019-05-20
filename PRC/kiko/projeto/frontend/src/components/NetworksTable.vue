935734978b644e172ea1aef931b45fb7858c5ae6
Kiko <fjmo_1997@hotmail.com>
2019-05-20 21:47:03 +0100

    [PRC] vue - NetworksTable done
    -can search titles
    -uses cards to save space
    -could be prettyer

--- PRC/kiko/projeto/frontend/src/components/NetworksTable.vue
+++ PRC/kiko/projeto/frontend/src/components/NetworksTable.vue
@@ -1,46 +1,52 @@
 <template>
   <v-container>
-    <v-card-title>
-      <v-text-field
-        v-model="searchText"
-        append-icon="search"
-        label="Search"
-        single-line
-      ></v-text-field>
-      <v-spacer/>
-      <v-text-field
-        v-model="pagination.page"
-        label="Page number"
-      ></v-text-field>
-    </v-card-title>
-    <!-- <p>{{networks}}</p> -->
-    <v-data-table
-      :headers="headers"
-      :items="networks"
-      :search="searchText"
-      :rows-per-page-items="rowsPerPage"
-      :pagination.sync="pagination"
-      class="elevation-1"
-    >
-      <template v-slot:no-data>
-        <v-alert :value="true" color="error" icon="warning">
-          Não foi possivel apresentar a lista de networks...
-        </v-alert>
-      </template>
-      <template v-slot:no-results>
-        <v-alert :value="true" color="error" icon="warning">
-          A pesquisa por "{{searchText}}" não teve resultados.
-        </v-alert>
-      </template>
 
-      <template v-slot:items="props">
-        <tr @click="rowClicked(props.item)">
-          <td class="subheading"> {{props.item.network.value.split('#NETWORK_')[1]}} </td>
-          <td v-if="props.item.label" class="subheading"> {{props.item.label.value}} </td>
-          <td v-else > <p><i>No Label</i></p> </td>
-        </tr>
-      </template>
-    </v-data-table>
+    <v-layout justify-center>
+      <v-flex xs12>
+        <v-toolbar color="indigo darken-2" dark>
+          <v-toolbar-title>Networks</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-flex xs6>
+            <v-text-field
+              v-model="searchText"
+              append-icon="search"
+              label="Search"
+              single-line
+            ></v-text-field>
+          </v-flex>
+        </v-toolbar>
+
+        <v-card>
+          <v-container fluid grid-list-md>
+            <v-layout row wrap>
+              <v-flex
+                v-for="card in filteredList"
+                :key="card.network.value"
+              >
+                <v-card
+                  flat hover
+                  dark color="grey darken-2"
+                  @click="itemClicked(card)">
+                    <v-container fill-height fluid pa-2>
+                      <v-layout fill-height>
+                        <v-flex align-end flexbox>
+                          <span class="title" v-text="fixName(card.network.value)"></span>
+                          <v-spacer/>
+                          <span class="subtitle" v-text="fixName(card.network.value)"></span>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <!-- <p>{{networks}}</p> -->
+
   </v-container>
 </template>
 
@@ -71,8 +77,19 @@
       }
     },
     methods: {
-      rowClicked: function (item) {
+      itemClicked: function (item) {
         this.$router.push('/networks/'+item.network.value.split('#NETWORK_')[1])
+      },
+      fixName: function (name) {
+        return name.split('#NETWORK_')[1]
+      }
+    },
+    computed: {
+      filteredList() {
+        return this.networks.filter(item => {
+          var name = item.network.value.split('#NETWORK_')[1]
+          return name.toLowerCase().includes(this.searchText.toLowerCase())
+        })
       }
     }
   }
