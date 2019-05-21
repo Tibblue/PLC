502c55d4bb4aadc8966c568ef1a69c3d5f22f11c
Kiko <fjmo_1997@hotmail.com>
2019-05-21 21:28:12 +0100

    [PRC] cardTble2 added + networkTable is now using it
    -cardTable2 is like cardTable but more generic
      -in the future it will replace cardTable entirely

--- PRC/kiko/projeto/frontend/src/views/TabelaNetwork.vue
+++ PRC/kiko/projeto/frontend/src/views/TabelaNetwork.vue
@@ -1,16 +1,49 @@
 <template>
-  <v-flex>
-    <NetworksTable />
-    <!-- <p>{{networks}}</p> -->
-  </v-flex>
+  <v-container>
+    <cardTable2
+      name="Networks"
+      :list="networksSimple"
+      route="networks"
+    ></cardTable2>
+  </v-container>
+
+  <!-- <h1> <mark> DEBUG </mark> </h1> -->
+  <!-- <p>{{networks}}</p> -->
+  <!-- <p>{{networksSimple}}</p> -->
+  <!-- <h1> <mark> DEBUG </mark> </h1> -->
+
 </template>
 
 <script>
-  import NetworksTable from '@/components/NetworksTable'
+  import cardTable2 from '@/components/cardTable2'
+  import axios from 'axios'
+  const lhost = "http://localhost:4005"
 
   export default {
     components: {
-      NetworksTable
-    }
+      cardTable2
+    },
+    data: () => ({
+      networks: [],
+      networksSimple: []
+    }),
+    mounted: async function (){
+      try{
+        var response = await axios.get(lhost+'/query/network_id');
+        this.networks = response.data.results.bindings
+        this.networksSimple = this.networks.map(this.simplify)
+      }
+      catch(e){
+        return(e);
+      }
+    },
+    methods: {
+      fixName: function (name) {
+        return name.split('#NETWORK_')[1].replace(/_/g, " ")
+      },
+      simplify: function (item) {
+        return {id:item.network.value.split('#NETWORK_')[1]}
+      }
+    },
   }
 </script>
