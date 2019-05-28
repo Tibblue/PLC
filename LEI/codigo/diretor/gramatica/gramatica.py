e8da448cf0c841890478decbc8d531c5b6ab26b4
raulvilasboas97 <raulvilasboas97@gmail.com>
2019-05-28 22:53:42 +0100

    [LEI] gramatica im progress -> GO KIKO

--- LEI/codigo/diretor/gramatica/gramatica.py
+++ LEI/codigo/diretor/gramatica/gramatica.py
@@ -1,10 +1,10 @@
 import sys
 from antlr4 import *
-from .gramaticaLexer import gramaticaLexer
-from .gramaticaParser import gramaticaParser
+from gramaticaLexer import gramaticaLexer
+from gramaticaParser import gramaticaParser
 
-def gramatica_check(ficheiro):
-    input = FileStream(ficheiro)
+def main(argv):
+    input = FileStream(argv[1])
     lexer = gramaticaLexer(input)
     stream = CommonTokenStream(lexer)
     parser = gramaticaParser(stream)
@@ -14,5 +14,4 @@ def gramatica_check(ficheiro):
     # print(parser)
     # print(tree)
 
-# if __name__ == '__main__':
-#     main(sys.argv)
+main(sys.argv)
--- LEI/codigo/diretor/read_dsl.py
+++ LEI/codigo/diretor/read_dsl.py
@@ -1,7 +1,7 @@
 import sys
+import os
 import re
 from operator import itemgetter
-from gramatica import gramatica
 
 # lê a dsl e retorna uma lista de tuplos que contém os bots e o dataset a ser usado
 def read_dsl():
@@ -10,48 +10,54 @@ def read_dsl():
     estados = []
     ficheiro = sys.argv[1]
 
-    gramatica.gramatica_check(ficheiro)
+    #TODO redirecionar output pra ficheiro e ver se o ficheiro esta vazio ou nao
+    cmd = os.popen('python3 gramatica/gramatica.py dsl.txt').read()
+    # cmd = os.popen('cd gramatica && python3 gramatica.py ../dsl.txt').read()
+    if(cmd is not None):
+        print("DSL está mal feita, Por Favor corriga-a.")
+        # print(cmd)
+        return None
+    else:
+        content = open(ficheiro).read()
+        content = content.split('\n')
 
-    content = open(ficheiro).read()
-    content = content.split('\n')
+        # trata de ver quais os bots no join e meter numa lista ornado por prioridade_bot
+        for linha in content:
+            linha = linha.split(' ')
+            # print(linha)
+            if linha[0] == 'JOIN':
+                for i in range(len(linha)-1):
+                    bot_number = re.search(r'b([0-9]+)',linha[i])
+                    prioridade_bot = re.search(r'!([0-9]+)',linha[i+1])
+                    if bot_number is not None:
+                        bot_number = int(bot_number.group(1))
+                        prioridade_bot = int(prioridade_bot.group(1))
+                        tuplo_joined = tuple((bot_number,prioridade_bot))
+                        tuplos_joined.append(tuplo_joined)
+            elif linha[0] == 'STATES':
+                estados = linha[1:]
+        tuplos_joined.sort(key=itemgetter(1),reverse=True)
 
-    # trata de ver quais os bots no join e meter numa lista ornado por prioridade_bot
-    for linha in content:
-        linha = linha.split(' ')
-        # print(linha)
-        if linha[0] == 'JOIN':
+        # pega na lista de bots ordenada e cria um tuplos com o nome do bot e uma lista com os datasets
+        for bot,prioridade_bot in tuplos_joined:
+            datasets = []
+            linha = content[bot]
+            linha = linha.split(' ')
+            # print(linha)
             for i in range(len(linha)-1):
-                bot_number = re.search(r'b([0-9]+)',linha[i])
-                prioridade_bot = re.search(r'!([0-9]+)',linha[i+1])
-                if bot_number is not None:
-                    bot_number = int(bot_number.group(1))
-                    prioridade_bot = int(prioridade_bot.group(1))
-                    tuplo_joined = tuple((bot_number,prioridade_bot))
-                    tuplos_joined.append(tuplo_joined)
-        elif linha[0] == 'STATES':
-            estados = linha[1:]
-    tuplos_joined.sort(key=itemgetter(1),reverse=True)
-
-    # pega na lista de bots ordenada e cria um tuplos com o nome do bot e uma lista com os datasets
-    for bot,prioridade_bot in tuplos_joined:
-        datasets = []
-        linha = content[bot]
-        linha = linha.split(' ')
-        # print(linha)
-        for i in range(len(linha)-1):
-                word = linha[i]
-                if word == 'CREATE':
-                    bot = linha[i+1]
-                if word == 'WITH':
-                    datasets.append(linha[i+1])
-                if word == 'FROM':
-                    datasets.append(linha[i+1])
-        if datasets != [] or bot != []:
-            triplo = tuple((bot,datasets,prioridade_bot))
-            triplos.append(triplo)
-    # print(estados)
-    # print(triplos)
-    return triplos,estados
+                    word = linha[i]
+                    if word == 'CREATE':
+                        bot = linha[i+1]
+                    if word == 'WITH':
+                        datasets.append(linha[i+1])
+                    if word == 'FROM':
+                        datasets.append(linha[i+1])
+            if datasets != [] or bot != []:
+                triplo = tuple((bot,datasets,prioridade_bot))
+                triplos.append(triplo)
+        # print(estados)
+        # print(triplos)
+        return triplos,estados
 
 read_dsl()
 
