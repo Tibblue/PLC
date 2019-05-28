2a8e6169c7ed2d5acd6dd05508917d7824f7cf26
Kiko <fjmo_1997@hotmail.com>
2019-05-28 13:08:49 +0100

    [PRC] simplify computed variables
    -computed filteredList is now reused by maxPage and pagedList for better performance
    -filter also changed to check both title and title_english

--- PRC/kiko/projeto/frontend/src/components/cardTable.vue
+++ PRC/kiko/projeto/frontend/src/components/cardTable.vue
@@ -65,8 +65,6 @@
               <v-layout fill-height px-2 pt-1>
                 <v-flex xs12 flexbox class="text-xs-center">
                   <span class="title">{{card.title}}</span>
-                  <v-spacer/>
-                  <span class="subtitle">{{card.title_japanese}}</span>
                   <v-spacer v-if="card.title_english"/>
                   <span v-if="card.title_english" class="subtitle">{{card.title_english}}</span>
                   </v-flex>
--- PRC/kiko/projeto/frontend/src/components/cardTableAnime.vue
+++ PRC/kiko/projeto/frontend/src/components/cardTableAnime.vue
@@ -119,30 +119,21 @@
     },
     computed: {
       maxPage() {
-        var filtered = this.list.filter(item => {
-          var name = item.title
-          return name.toLowerCase().includes(this.searchText.toLowerCase())
-        })
-        return Math.ceil(filtered.length/this.pageSize)
+        return Math.ceil(this.filteredList.length/this.pageSize)
       },
-      // pagedListComp: this.pagedList(),
       filteredList() {
         return this.list.filter(item => {
-          var name = item.title
-          return name.toLowerCase().includes(this.searchText.toLowerCase())
+          var title = item.title.toLowerCase()
+          var title_english = item.title_english.toLowerCase()
+          var search_text = this.searchText.toLowerCase()
+          return title.includes(search_text) | title_english.includes(search_text)
         })
       },
       pagedList() {
-        var filtered = this.list.filter((item) => {
-          var name = item.title
-          // var title_english = item.title_english
-          return name.toLowerCase().includes(this.searchText.toLowerCase())
-        })
-        var paged = filtered.filter((item,index) => {
+        return this.filteredList.filter((item,index) => {
           return index>=(this.currentPage-1)*this.pageSize
                   && index<this.currentPage*this.pageSize
         })
-        return paged
       }
     }
   }
