314c1cf62e10f9ad9303b7a17b6fc10bbf4016c4
Raul Vilas Boas <raulvilasboas97@gmail.com>
2019-05-31 14:50:30 +0100

    [SPLN] exer

--- SPLN/testes/teste_jan_19/raul/questao4.py
+++ SPLN/testes/teste_jan_19/raul/questao4.py
@@ -5,8 +5,7 @@ textos = [
     "a b c a a a a a a b d e e d a e d batatas",
     "batatas a b c a a c c c c c c b d e e d a e d",
     "a b a b e e e e d f b a c a c c batatas a c a c c a c b c b c b c b",
-        "a b a b e batatas a b b b e e a d f c a c c a c b c b c b c b",
-
+    "a b a b e batatas a b b b e e a d f c a c c a c b c b c b c b",
     ]
 
 def n_gram(texto,n):
@@ -25,26 +24,26 @@ def find(palavra,pal_polaridade,textos,n):
         for ngram in ngrams:
             if 'batatas' in ngram:
                 ngrams_com_palavra.append(ngram)
-        meio = n / 2
+
+        if n % 2 == 0:
+            meio = round((n / 2) - 1)
+        else:
+            meio = round(n/2)
         for ngram in ngrams_com_palavra:
-            for meio in range(len(ngram)):
+            for i in range(meio,len(ngram)): 
                 if ngram[meio] == 'batatas':
                     print(ngram)
+            # print(len(ngram))
+            # for meio in range(len(ngram)):
+            #     if ngram[meio] == 'batatas':
+            #         print(ngram)
                     
         # print(ngrams_com_palavra)
         # print()
 
-# find('batatas',pal_polaridade,textos,5)
-
-lista = ["-1","0","1"]
-
-def count_char_occur(lista):
-  occurs = {}
-  for l in lista:
-    occurs[l] = random.choice(lista)
-  return occurs
+find('batatas',dic_polaridade,textos,10)
 
 # print(count_char_occur(textos[0]))
 # count_char_occur(string)
 
-print(dic_polaridade)
+# print(dic_polaridade)
